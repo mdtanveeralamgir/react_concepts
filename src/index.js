1371ba62cd524406ca219d77911c78e27905b76e
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-08-07 00:17:48 -0400

    Function components

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 /*
+Components and Props
 Source:
+- Basic of components and props:
+https://reactjs.org/docs/components-and-props.html#function-and-class-components
+- Detailed component API reference: 
+https://reactjs.org/docs/react-component.html
+
 */
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+//Function components
+function Welcome(props) //props = properties
+{
+    return <h2>Hello, {props.name}</h2>;
+}
+
+const root = ReactDOM.createRoot(
+    document.getElementById('root')
+);
+
+const element = <Welcome name="Sara" />;
+root.render(element);
+
+//Class components
+// class Welcome extends React.Component
+// {
+//     render()
+//     {
+//         return <h1>Hello, {this.props.name}</h1>
+//     }
+// }
+
