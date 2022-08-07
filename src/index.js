d3eb8e4c37339e773def7996dce968d2412145e2
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-08-07 01:26:02 -0400

    clock with function

--- src/index.js
+++ src/index.js
@@ -3,3 +3,24 @@ State and Lifecycle
 Source:
 https://reactjs.org/docs/state-and-lifecycle.html
 */
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function Clock(props)
+{
+    return (
+        <div>
+            <h1>It is {props.date.toLocaleTimeString()}</h1>
+        </div>
+    );
+}
+
+function tick()
+{
+    root.render(<Clock date={new Date()} />);
+}
+
+setInterval(tick, 1000);
