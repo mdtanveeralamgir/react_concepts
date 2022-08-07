5d2176a8f81460dc082aac6ce481f857b0620a7f
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-08-07 01:52:15 -0400

    class component, lifecycle  methods

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ https://reactjs.org/docs/state-and-lifecycle.html
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+/*
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 function Clock(props)
@@ -24,3 +25,56 @@ function tick()
 }
 
 setInterval(tick, 1000);
+
+*/
+
+//Concerting the above function into a class
+
+
+class Clock extends React.Component
+{
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            date: new Date().toLocaleTimeString(),
+        }
+    }
+
+    //Lifecycle methods
+
+    //Mounting: when the clock renderes to the DOM first time
+    componentDidMount()
+    {
+        this.timeID = setInterval(
+            ()=> this.tick(),
+            1000
+        );
+    }
+
+    //Unmount: DOM produces by the clock is removed
+    componentWillUnmount()
+    {
+        clearInterval(this.timeID);
+    }
+
+    tick()
+    {
+        this.setState({
+            date: new Date().toLocaleTimeString(),
+        });
+    }
+
+    render()
+    {
+        return(
+            <div>
+                <h1>It is {this.state.date}</h1>
+            </div>
+        )
+    }
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<Clock />);
