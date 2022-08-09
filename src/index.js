c6ddbb57ee7bebc9d7f6fcf53a7a682cec4d0245
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-08-09 11:48:57 -0400

    passing state value to outside func

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 /*
+Lifting State Up
 Source:
+https://reactjs.org/docs/lifting-state-up.html
 */
 
 import React from 'react';
@@ -7,9 +9,43 @@ import ReactDOM from 'react-dom/client';
 
 
 
+function BoilingVerdict(props)
+{
+    const temperature = props.celsius;
+    if(temperature >= 100)
+        return <p>The water would boil</p>;
+    
+        return <p>The water would not boil</p>;
+}
+
+class Calculator extends React.Component
+{
+    constructor(props)
+    {
+        super(props);
+        this.state = {temperature: ''};
+    }
+
+    handleChance = (event)=>
+    {
+        this.setState({temperature: event.target.value});
+    }
+
+    render()
+    {
+        const temperature = this.state.temperature;
+        return(
+            <fieldset>
+                <legend>Enter temperature here.</legend>
+                <input value={temperature} onChange={this.handleChance}></input>
+                <BoilingVerdict celsius={parseFloat(temperature)} />
+            </fieldset>
+        );
+    }
+}
 
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Toggle />);
+root.render(<Calculator />);
