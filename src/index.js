1eefa847e56867fd72791db9ceae3717b90766c3
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-08-18 23:28:33 -0400

    added few important comments about finding states and placing them in components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,42 @@
 /*
+Thinking in react
 Source:
+https://reactjs.org/docs/thinking-in-react.html
 */
+/*
+Important notes:
 
+Identify The Minimal (but complete) Representation Of UI State:
+
+To make your UI interactive, you need to be able to trigger changes to your underlying data model. React achieves this with state.
+To build your app correctly, you first need to think of the minimal set of mutable state that your app needs. The key here is DRY: Don’t Repeat Yourself. Figure out the absolute minimal representation of the state your application needs and compute everything else you need on-demand. For example, if you’re building a TODO list, keep an array of the TODO items around; don’t keep a separate state variable for the count. Instead, when you want to render the TODO count, take the length of the TODO items array.
+Think of all the pieces of data in our example application. We have:
+The original list of products
+The search text the user has entered
+The value of the checkbox
+The filtered list of products
+Let’s go through each one and figure out which one is state. Ask three questions about each piece of data:
+Is it passed in from a parent via props? If so, it probably isn’t state.
+Does it remain unchanged over time? If so, it probably isn’t state.
+Can you compute it based on any other state or props in your component? If so, it isn’t state.
+The original list of products is passed in as props, so that’s not state. The search text and the checkbox seem to be state since they change over time and can’t be computed from anything. And finally, the filtered list of products isn’t state because it can be computed by combining the original list of products with the search text and value of the checkbox.
+So finally, our state is:
+The search text the user has entered
+The value of the checkbox
+
+Identify Where Your State Should Live:
+
+For each piece of state in your application:
+Identify every component that renders something based on that state.
+Find a common owner component (a single component above all the components that need the state in the hierarchy).
+Either the common owner or another component higher up in the hierarchy should own the state.
+If you can’t find a component where it makes sense to own the state, create a new component solely for holding the state and add it somewhere in the hierarchy above the common owner component.
+Let’s run through this strategy for our application:
+ProductTable needs to filter the product list based on state and SearchBar needs to display the search text and checked state.
+The common owner component is FilterableProductTable.
+It conceptually makes sense for the filter text and checked value to live in FilterableProductTable
+
+*/
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
